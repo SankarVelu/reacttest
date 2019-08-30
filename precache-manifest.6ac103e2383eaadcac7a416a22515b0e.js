55156d657a6862f75755d788287903143883d481
Sankar <sankar.hexamarvel@gmail.com>
2019-08-30 14:45:13 +0530

    Updates

---
+++ precache-manifest.6ac103e2383eaadcac7a416a22515b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbb0aaac79df37d3d562bcca54c6642",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "58cc7dc9b0ae091e9061",
+    "url": "/reacttest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f9b733e8a5e0bd152c80",
+    "url": "/reacttest/static/js/2.3faf83df.chunk.js"
+  },
+  {
+    "revision": "58cc7dc9b0ae091e9061",
+    "url": "/reacttest/static/js/main.574d9b7d.chunk.js"
+  },
+  {
+    "revision": "4c1a927e5008f7f26aff",
+    "url": "/reacttest/static/js/runtime~main.f474146d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttest/static/media/logo.5d5d9eef.svg"
+  }
+]);
